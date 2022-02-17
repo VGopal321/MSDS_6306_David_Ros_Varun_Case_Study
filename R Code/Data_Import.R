e864a50ebc3e1a6d5e6d543f5ef7c6ec86670472
VGopal321 <96988452+VGopal321@users.noreply.github.com>
2022-02-16 22:09:10 -0500

    Update Data_Import.R

--- R Code/Data_Import.R
+++ R Code/Data_Import.R
@@ -1,1 +1,21 @@
-#Data Import
+#Libraries
+library(tidyverse)
+library(caret)
+library(class)
+library(e1071)
+
+#Import datasets
+beers=read.csv(file.choose(),header=TRUE)
+breweries=read.csv(file.choose(),header=TRUE)
+
+#Filter out NAs in beers
+beersclean=beers%>%filter(!(is.na(IBU)|is.na(ABV)))
+
+#Find how many breweries there are per state
+breweries%>%
+  group_by(State)%>%
+  summarize(count=n())%>%
+  ggplot(aes(x=State,y=count))+
+  geom_bar(stat='identity')
+  
+
