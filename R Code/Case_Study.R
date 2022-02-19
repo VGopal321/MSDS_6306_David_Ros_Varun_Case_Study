1356aad75ca0cfa5ee23b8ad2ac7139582e23192
VGopal321 <96988452+VGopal321@users.noreply.github.com>
2022-02-19 13:59:34 -0500

    Update Case_Study.R

--- R Code/Case_Study.R
+++ R Code/Case_Study.R
@@ -21,7 +21,10 @@ breweries%>%
   group_by(State)%>%
   summarize(count=n())%>% 
   ggplot(aes(x=State,y=count))+
-  geom_bar(stat='identity')
+  geom_bar(stat='identity')+
+  ggtitle('Number of Breweries per State')+
+  xlab('State')+
+  ylab('Number of Breweries')
 
 #Create Heat Map 
 FiftyStates=data.frame(State=state.abb,Name=state.name) #Create and Name columns of DF
@@ -49,8 +52,6 @@ map.df%>%ggplot(aes(x=long,y=lat,group=group))+
 beersclean$Brew_ID=beersclean$Brewery_id
 beerbreweries=merge(beersclean,breweries,by="Brew_ID",all.x = TRUE)
 beerbreweries=beerbreweries%>%select(!Brewery_id)
-dim(beersclean)
-dim(beerbreweries)
 
 #Find average ABV and IBU per state
 head(beerbreweries)
@@ -58,12 +59,35 @@ colnames(beerbreweries)[8]='Brewery_Name'
 colnames(beerbreweries)[2]='Beer_Name'
 
 #Gather mean abv and ibu per each state
-ABVIBUData=beerbreweries%>%group_by(State)%>%summarize(meanABV=mean(ABV),meanIBU=mean(IBU),count=n())
+ABVIBUData=beerbreweries%>%group_by(State)%>%summarize(medianABV=median(ABV),medianIBU=median(IBU),count=n())
 
 #barplot ABV
-ABVplot=ABVIBUData%>%ggplot(aes(x=State,y=meanABV))+geom_bar(stat='identity')
+ABVplot=ABVIBUData%>%ggplot(aes(x=State,y=medianABV))+
+  geom_bar(stat='identity')+
+  ggtitle('Median ABV by State')+
+  ylab('Median ABV')
 ggplotly(ABVplot)
 
 #barplot IBU
-IBUplot=ABVIBUData%>%ggplot(aes(x=State,y=meanIBU))+geom_bar(stat='identity')
+IBUplot=ABVIBUData%>%ggplot(aes(x=State,y=medianIBU))+
+  geom_bar(stat='identity')+
+  ggtitle('Median IBU by State')+
+  ylab('Median IBU')
 ggplotly(IBUplot)
+
+#Find State w/ Max ABV and IBU
+ABVIBUData%>%filter(medianABV==max(medianABV))
+ABVIBUData%>%filter(medianIBU==max(medianIBU))
+#Maine has the highest ABV and IBU
+
+#Summary Stats for ABV
+summary(beerbreweries$ABV)
+
+#Scatter Plot ABV and IBU
+beerbreweries%>%ggplot(aes(x=ABV,y=IBU))+
+  geom_point()+geom_smooth(method="lm")
+
+#k-NN for IPAs and other ales
+AleData=beerbreweries%>%filter(grepl("(Ale|IPA)",beerbreweries$Beer_Name,ignore.case = TRUE))
+
+AleData$IPAorALE=ifelse(grepl("IPA",AleData$Beer_Name),"IPA","Ale")
